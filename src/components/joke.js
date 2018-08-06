71d8a5481890a5f35f775f2799d4613f7e0153b8
David Gilbert <d_gilbert5@yahoo.com>
2018-08-06 00:10:08 -0400

    first commit

---
+++ src/components/joke.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Chuck from "chucknorris-io";
+
+const chuckApi = new Chuck();
+
+export default class Joke extends React.Component {
+  state = { joke: null, loading: false, url: null };
+
+  fetchJoke = async () => {
+    this.setState({ loading: true });
+
+    const categories = await chuckApi.getJokeCategories();
+    console.log("Original category", categories);
+
+    // filter out the "explicit" category
+    const filteredCategories = categories.filter( (category) => category !== 'explicit');
+    console.log("Filtered category", filteredCategories);
+
+    // get a random category
+    const category = filteredCategories[Math.floor(Math.random()*filteredCategories.length)];
+    console.log("Random category", category);
+
+    const res = await chuckApi.getRandomJoke(category);
+    console.log(res);
+    this.setState({ joke: res.value, url: res.sourceUrl, loading: false });
+  };
+
+  componentDidMount() {
+    this.fetchJoke();
+  }
+
+  render() {
+    return (
+      <section className="hero is-primary">
+      <div className="hero-body">
+        <div className="container">
+          <div className="joke">{this.state.joke}</div>
+        </div>
+        <button className="btn" onClick={this.fetchJoke}>Get Joke</button>
+      </div>
+    </section>
+    )
+  }
+}
